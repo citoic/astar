111430b7e02a8caa30c6ae8c14c736710eb5e1aa
Edward C <ciotic@gmail.com>
2014-09-11 20:27:56 -0700

    added more to node class and added code to print the grid, though it doesnt work yet

--- food_agent.py
+++ food_agent.py
@@ -3,7 +3,8 @@ import sys
 class Node:
 
     def __init__(self):
-        self.kind = 0 #Can either be 0, 1, or 2 meaning empty, wall, or ramen
+        self.kind = -1 #Can either be 0, 1, or 2 meaning empty, wall, or ramen
+        self.has_person = 0 #for knowing the current location
         self.g = 0 #g value for a*
         self.h = 0 #h value for a*
         self.f = 0 #f value for a*
@@ -13,7 +14,7 @@ class Node:
         return self.kind
 
     def set_kind(self, kind):
-        self.kind = k
+        self.kind = kind
 
     def get_g(self):
         return self.g
@@ -39,6 +40,28 @@ class Node:
     def set_paret(self, parent):
         self.parent = parent
 
+    def set_occupied(self, x):
+        self.has_person = x
+
+    def get_occupied(self):
+        return self.has_person
+
+#takes grid as input, and prints to screen what original input file looks like but with updated locations for the '@' symbol
+def display_grid():
+    for n in range(0, len(grid)):
+        line_str = ''
+        for x in range(0, len(grid[n])):
+            if grid[n][x].get_occupied == 1:
+                line_str += '@'
+            elif  grid[n][x].get_kind == 0:
+                line_str = line_str + '.'
+                print 'omg'
+            elif  grid[n][x].get_kind == 1:
+                line_str = line_str + '#'
+            elif  grid[n][x].get_kind == 2:
+                line_str = line_str + '%'
+        print line_str
+        print 'doing something'
 
 
 #file IO. Taken from lecture slides
@@ -50,13 +73,41 @@ f = open(sys.argv[1]) #program name is argv[0]
 lines = f.readlines() # reads all lines into list one go
 f.close()
 
-
-
-
+#this cleans up the lines and makes each line into a list itself. This will be used to cunstruct the list of nodes
 for n in range(0, len(lines)):
     lines[n] = list(lines[n].strip()) 
 
 
+print lines #for debug purposes currently 
 
 
-print lines
+grid = []
+
+for n in range(0, len(lines)):
+    li = []
+    for x in range(0, len(lines[n])):
+        node = Node()
+        if lines[n][x] == '.' or lines[n][x] == '@':
+            node.set_kind(0)
+        if lines[n][x] == '#':
+            node.set_kind(1)
+        if lines[n][x] == '%':
+            node.set_kind(2)
+        #TODO: set other node properties
+        li.append(node)
+    grid.append(li)
+
+
+#debug stuff
+for n in range(0, len(grid)):
+    for x in range(0, len(grid[n])):
+        print grid[n][x].get_kind()
+
+
+display_grid() #doesnt work yet. mostly because how do I python 
+
+
+
+
+
+
