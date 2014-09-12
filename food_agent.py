c44a71fb7e81fee650bf9c3f4ef2b56c57aed918
Edward C <ciotic@gmail.com>
2014-09-11 19:41:01 -0700

    added getters and setters to node

--- food_agent.py
+++ food_agent.py
@@ -1,5 +1,45 @@
 import sys
 
+class Node:
+
+    def __init__(self):
+        self.kind = 0 #Can either be 0, 1, or 2 meaning empty, wall, or ramen
+        self.g = 0 #g value for a*
+        self.h = 0 #h value for a*
+        self.f = 0 #f value for a*
+        self.parent = [0, 0] #for determining path; may not need since every step is required to be drawn
+
+    def get_kind(self):
+        return self.kind
+
+    def set_kind(self, kind):
+        self.kind = k
+
+    def get_g(self):
+        return self.g
+
+    def set_g(self, g):
+        self.g = g
+
+    def get_h(self):
+        return self.h
+
+    def set_h(self, h):
+        self.h = h
+
+    def get_f(self):
+        return self.f
+
+    def set_f(self, f):
+        self.f = f
+
+    def get_parent(self):
+        return self.parent
+
+    def set_paret(self, parent):
+        self.parent = parent
+
+
 
 #file IO. Taken from lecture slides
 if(len(sys.argv)) != 2: 
@@ -11,9 +51,14 @@ lines = f.readlines() # reads all lines into list one go
 f.close()
 
 
+
+
 for n in range(0, len(lines)):
     lines[n] = list(lines[n].strip()) 
 
-print lines
+grid = [len(lines)][len(lines[0])] #this assumes that the input is either a single line or a perfect rectangle
 
-#fval = 
+
+for n in range(0, len(lines)):
+
+print lines
